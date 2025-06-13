496cf64bb9f6f47cc59caec15acbc622565dc24f
Fauzy169 <ahmadfauzy169@gmail.com>
2025-06-13 14:09:24 +0800

    Commmit css fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Hapus wrapping JSON agar response rapi
         JsonResource::withoutWrapping();
+
+        // Paksa semua URL menggunakan HTTPS di environment production
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
